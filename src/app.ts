4690d0fa7880f27c29b5e8709be68e73d3840ac8
KD <kardav2019@gmail.com>
2026-02-23 14:49:42 +0200

    feat: Redis
    - Better health endpoints
    - Better error handling

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import ApiResponse from './utils/ApiResponse';
 import { globalErrorHandler } from './middleware/ErrorHandler';
 import { metricsEndpoint, metricsMiddleware } from './middleware/observability';
 import { requestLogger } from './configs/observability';
+import { redisClient } from './configs/redis';
+import { logger } from './utils/logger';
 
 const app = express();
 
@@ -16,8 +18,42 @@ app.use(metricsMiddleware);
 
 app.get('/metrics', metricsEndpoint);
 
-app.get('/health', (_, res: Response) => {
-  ApiResponse.success(res, 200, 'Healthy');
+app.get('/health/live', (_req, res) => {
+  return ApiResponse.success(res, 200, 'Process is alive', {
+    uptime: Math.floor(process.uptime()),
+  });
+});
+
+app.get('/health', async (_req, res) => {
+  let redisStatus = 'down';
+  try {
+    if (redisClient.isOpen) {
+      await redisClient.ping();
+      redisStatus = 'up';
+    }
+  } catch (err) {
+    logger.error({ err }, 'Redis ping failed during health check');
+  }
+
+  const isSystemUp = redisStatus === 'up';
+
+  const healthData = {
+    services: {
+      redis: {
+        status: redisStatus,
+      },
+    },
+    uptime: `${Math.floor(process.uptime())}s`,
+    timestamp: new Date().toISOString(),
+  };
+
+  if (!isSystemUp) {
+    logger.warn(healthData, 'Health check failed');
+
+    return ApiResponse.error(res, 503, 'Service Unavailable', [healthData.services]);
+  }
+
+  return ApiResponse.success(res, 200, 'System is healthy', healthData);
 });
 
 app.use((_, res: Response) => {
